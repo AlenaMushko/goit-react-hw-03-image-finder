a1f65b4aa779880bd679f0891a0136343f0f2edb
alena <evstafienko_a@ukr.net>
2023-02-12 09:55:19 +0200

    end

--- src/components/EndImg/EndImg.js
+++ src/components/EndImg/EndImg.js
@@ -1,12 +1,7 @@
-import { Component } from 'react';
-
-export class EndImg extends Component {
-  render() {
-    const { searchValue } = this.props;
-    return (
-      <p style={{ textAlign: 'center', color: 'blue', fontSize: '24px' }}>
-        Images is end
-      </p>
-    );
-  }
+export function EndImg() {
+  return (
+    <p style={{ textAlign: 'center', color: 'blue', fontSize: '24px' }}>
+      Images is end
+    </p>
+  );
 }
